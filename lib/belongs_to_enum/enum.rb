0c3aec6f5a3a773db4c9958137e10aff2e27ccd2
Jeremy Walker <jez.walker@gmail.com>
2011-05-17 20:25:39 +0100

    Added code used in existing projects

---
+++ lib/belongs_to_enum/enum.rb
@@ -0,0 +1,75 @@
+module BelongsToEnum
+  class Enum
+    def Enum.add_item(key,value)
+      @hash ||= {}
+      @hash[key]=value
+    end
+
+    def Enum.get_from_key(key)
+      @hash[key]
+    end
+
+    def Enum.method_missing(key, *args)
+      @hash[key] || super
+    end
+
+    def Enum.each
+      @hash.each {|key,value| yield(key,value)}
+    end
+
+    def Enum.map
+      @hash.map {|key,value| yield(key,value)}
+    end
+
+    def Enum.keys
+      @hash.values
+    end
+
+    def Enum.items
+      @hash.sort{|a,b| a[1]<=>b[1]}.map{|i|[i[0].to_s.gsub("_", " ").capitalize,i[1]]}
+    end
+
+    def Enum.get(value)
+      @hash.key(value).to_s
+    end
+
+    def Enum.display(value)
+      get(value).gsub("_", " ").titleize
+    end
+
+    def Enum.raw_hash
+      @hash
+    end
+
+    def self.create(name, keys)
+      
+      # Check to see if this has already been defined...
+      begin
+        "#{self.name}::#{name.to_s.camelize}".constantize
+        return
+      rescue
+      end
+
+      klass = self.const_set name.to_s.camelize, Class.new(Enum)
+
+      if keys.respond_to? :raw_hash
+        keys.raw_hash.each do |k,v|
+          klass.add_item(k,v)
+        end
+
+      elsif(keys.is_a? Hash)
+        keys.each do |k,v|
+          klass.add_item(k,v)
+        end
+      #elsif(keys.respond_to? :raw_hash)
+      #  keys.send(:raw_hash).each do |k,v|
+      #    klass.add_item(k,v)
+      #  end
+      else
+        keys.each_with_index do |key, index|
+          klass.add_item(key, index + 1)
+        end
+      end
+    end
+  end
+end
--- lib/belongs_to_enum/hook.rb
+++ lib/belongs_to_enum/hook.rb
@@ -1,6 +1,17 @@
 module BelongsToEnum
   module Hook
-    def belongs_to_enum
+    def belongs_to_enum(name, keys)
+          create_enum(name, keys)
+      name = name.to_s
+      class_eval <<-EOS
+        def #{name}?
+          #{name}_id.to_i > 0
+        end
+
+        def #{name}
+          #{name.camelize}.display(#{name}_id)
+        end
+      EOS
     end
   end
 end
