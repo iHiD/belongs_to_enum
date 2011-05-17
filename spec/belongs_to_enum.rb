cc3f53807cbf3008981131f4479d8d9ac412d716
Jeremy Walker <jez.walker@gmail.com>
2011-05-17 22:44:44 +0100

    Keys can be array or hash

--- spec/belongs_to_enum.rb
+++ spec/belongs_to_enum.rb
@@ -9,5 +9,13 @@ describe "Belongs To Enum models" do
   it "constant has keys" do
     User::Role.should respond_to(:admin)
   end
+  
+  it "keys should start at 1" do
+    User::Role.normal.should == 1
+  end
+
+  it "keys should respect hash values" do
+    Vehicle::Make.honda.should == 10
+  end
 
 end
